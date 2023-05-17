2033941e63cecb41fb42f04665bbcf81f2437088
Bambyboi <samuelkaleopa9@gmail.com>
2023-05-17 16:01:06 -0700

    section 15 part one with error

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
-using API.Errors;
 using API.Extensions;
 using API.Middleware;
-using Core.Interfaces;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Data.Identity;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,12 +14,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 
 
 app.UseSwagger();
@@ -27,7 +31,9 @@ app.UseStaticFiles();
 
 app.UseCors("CorsPolicy");
 //error cors policy
-app.UseCors(options => options.AllowAnyOrigin());  
+app.UseCors(options => options.AllowAnyOrigin()); 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -36,11 +42,15 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
 }
 catch (Exception ex)
 {
